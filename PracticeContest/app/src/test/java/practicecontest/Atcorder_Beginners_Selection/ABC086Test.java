7b613e32df9cccdb397d15030e48095fc99a2fa0
Shiryu_Go <ilovenhunhu777@gmail.com>
2023-11-15 07:24:40 +0900

    Add : テストの追加 https://atcoder.jp/contests/abs/tasks/abc086_a 現状、テストが通っていない

---
+++ PracticeContest/app/src/test/java/practicecontest/Atcorder_Beginners_Selection/ABC086Test.java
@@ -0,0 +1,41 @@
+package practicecontest.Atcorder_Beginners_Selection;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import 出力関係.StandardInputStream;
+import 出力関係.StandardOutputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ABC086Test {
+    private final StandardInputStream in = new StandardInputStream();
+    private final StandardOutputStream out = new StandardOutputStream();
+
+    @BeforeEach
+    public void before() {
+        System.setIn(in);
+        System.setOut(out);
+    }
+
+    @AfterEach
+    public void after() {
+        System.setIn(null);
+        System.setOut(null);
+    }
+
+    @Test
+    public void main01() {
+        in.inputln("3 4" );
+        ABC086A.main(null);
+        assertThat(out.readLine()).isEqualTo("Even");
+    }
+    @Test
+    public void main02(){
+        in.inputln("1 21");
+        ABC086A.main(null);
+        assertThat(out.readLine()).isEqualTo("Odd");
+    }
+
+}
