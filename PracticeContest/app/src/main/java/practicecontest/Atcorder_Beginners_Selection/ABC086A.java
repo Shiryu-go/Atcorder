c930facad1f32eb2eae41da9ee156913aa642577
Shiryu_Go <ilovenhunhu777@gmail.com>
2023-11-15 07:26:53 +0900

    refactor : 足してた。かけてください。

--- PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC086A.java
+++ PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC086A.java
@@ -7,7 +7,7 @@ public class ABC086A {
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)) {
             int number = sc.nextInt();
-            number += sc.nextInt();
+            number *= sc.nextInt();
             if(number %2 == 0){
                 System.out.println("Even");
             } else if (number %2==1) {
