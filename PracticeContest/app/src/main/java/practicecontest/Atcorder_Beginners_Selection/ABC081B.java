2ba94c025f451250c3bec10b16b3c19c3589d8d9
Shiryu_Go <ilovenhunhu777@gmail.com>
2023-11-17 07:08:02 +0900

    Bug : java.util.NoSuchElementException これ困りがちだ…。何を理解できていないのかな。

--- PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC081B.java
+++ PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC081B.java
@@ -1,8 +1,38 @@
 package practicecontest.Atcorder_Beginners_Selection;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /*
 https://atcoder.jp/contests/abs/tasks/abc081_b
  */
 public class ABC081B {
+    public static void main(String[] args){
+        List<Integer> list = new ArrayList<>();
+        int 数字を2で割れる回数 = 0;
+        boolean 繰り返し = true;
+        try(Scanner sc = new Scanner(System.in)){
+            int 数字を読み込む回数 = sc.nextInt();
+            for(int i = 0 ; i <= 数字を読み込む回数 ; i++){
+                list.add(sc.nextInt());
+            }
+            while(繰り返し){
+                for(int i = 0 ; i <= list.size() ; i++){
+                    if(list.get(i)%2 == 0){
+                        list.set(i,list.get(i)/2);
+                    }else{
+                        繰り返し = false;
+                        break;
+                    }
+                    if(i== list.size()){
+                        数字を2で割れる回数++;
+                    }
+
+                }
+            }
+            System.out.println(数字を2で割れる回数);
+        }
+    }
 
 }
