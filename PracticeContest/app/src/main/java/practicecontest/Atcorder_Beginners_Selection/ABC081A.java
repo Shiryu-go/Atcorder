6160388b7b34f34ae66ced6449240e02e0648f78
Shiryu_Go <ilovenhunhu777@gmail.com>
2023-11-16 21:49:31 +0900

    Refactor : どうパースするかわからん。 Bug有り

--- PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC081A.java
+++ PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC081A.java
@@ -1,4 +1,21 @@
 package practicecontest.Atcorder_Beginners_Selection;
 
+import java.util.Scanner;
+
 public class ABC081A {
+    public static  void main(String[] args){
+        int number = 0;
+        try(Scanner sc = new Scanner(System.in)){
+            if(sc.nextInt() == 1){
+                number++;
+            }
+            if(sc.nextInt() == 1){
+                number++;
+            }
+            if(sc.nextInt() == 1){
+                number++;
+            }
+            System.out.print(number);
+        }
+    }
 }
