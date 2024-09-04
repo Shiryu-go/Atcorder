19bada90e5bb79cfefff7101760e084a3d052cfb
Shiryu_Go <ilovenhunhu777@gmail.com>
2024-09-05 00:51:54 +0900

    一件テストをつうかできるようにした

--- PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC087B.java
+++ PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC087B.java
@@ -1,7 +1,12 @@
 package practicecontest.Atcorder_Beginners_Selection;
 
+import java.util.Scanner;
+
 public class ABC087B {
     public static void main(String[] args) {
-
+        try(Scanner sc = new Scanner(System.in)) {
+            int number = sc.nextInt();
+            System.out.println(number);
+        }
     }
 }
