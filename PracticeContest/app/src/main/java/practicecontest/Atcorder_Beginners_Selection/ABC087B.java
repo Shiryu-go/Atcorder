d4240d88734551095f56fad189f56c576c9d3dcb
Shiryu_Go <ilovenhunhu777@gmail.com>
2024-09-06 01:03:17 +0900

    最低限のモデリングを行った。

--- PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC087B.java
+++ PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC087B.java
@@ -10,3 +10,20 @@ public class ABC087B {
         }
     }
 }
+
+enum 硬貨{
+    五百円硬貨(500),
+    百円硬貨(100),
+    五十円硬貨(100),
+        ;
+    private final Integer 金額;
+
+    硬貨(int i) {
+        金額 = i;
+    }
+};
+
+record 金(
+    硬貨 coin
+    ,int number
+){}
