05c349ec0ffee812ce912f956a61b2652dfd90bb
Shiryu_Go <ilovenhunhu777@gmail.com>
2024-09-10 23:37:12 +0900

    入力値をRecord型で保存するの、めっちゃ良くね？
    Atcorderであれば、入力値って一回入ったら不変で、インタラクティブではないからさ。
    天才的発想

--- PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC087B.java
+++ PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC087B.java
@@ -5,12 +5,17 @@ import 汎用.硬貨;
 public class ABC087B {
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)) {
-            int number = sc.nextInt();
-            System.out.println(number);
+            入力値 input = new 入力値(sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt());
+            System.out.println(input.五百円玉の枚数());
         }
     }
 }
-
+record 入力値(
+  int 五百円玉の枚数,
+  int 百円玉の枚数,
+  int 五十円玉の枚数,
+  int 合計金額
+){}
 
 record 金(
     硬貨 coin
