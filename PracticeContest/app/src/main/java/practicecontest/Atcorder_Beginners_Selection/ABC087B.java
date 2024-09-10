3adf34196c01edc35158637d9192135bf19cb2be
Shiryu_Go <ilovenhunhu777@gmail.com>
2024-09-11 01:01:57 +0900

    インターフェース設計、アツすぎる
    仕事じゃないと楽しすぎる

--- PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC087B.java
+++ PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC087B.java
@@ -1,5 +1,7 @@
 package practicecontest.Atcorder_Beginners_Selection;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import 汎用.硬貨;
 public class ABC087B {
@@ -9,13 +11,32 @@ public class ABC087B {
             System.out.println(input.五百円玉の枚数());
         }
     }
+
+    public 回答のリスト 全件網羅() {
+        List<回答> list = new ArrayList<>();
+        return new 回答のリスト(list);
+    }
 }
+
+
 record 入力値(
   int 五百円玉の枚数,
   int 百円玉の枚数,
   int 五十円玉の枚数,
   int 合計金額
 ){}
+
+record 回答のリスト(
+  List<回答> 回答List
+){}
+
+record 回答(
+        int 五百円玉の枚数,
+        int 百円玉の枚数,
+        int 五十円玉の枚数
+){}
+
+
 
 record 金(
     硬貨 coin
