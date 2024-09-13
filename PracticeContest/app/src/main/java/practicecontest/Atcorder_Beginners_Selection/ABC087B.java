c7301b2c02b08aa3fc45174a0d17c5ffa7d06d39
Shiryu_Go <ilovenhunhu777@gmail.com>
2024-09-13 21:13:56 +0900

    ループを作った

--- PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC087B.java
+++ PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC087B.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import 汎用.硬貨;
+
+import static 汎用.硬貨.*;
+
 public class ABC087B {
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)) {
@@ -12,9 +15,20 @@ public class ABC087B {
         }
     }
 
-    public 回答のリスト 全件網羅() {
+    public 回答のリスト 全件網羅(入力値 input) {
         List<計算結果と内容> list = new ArrayList<>();
-        return new 回答のリスト(list);
+        for(int 五十円玉枚数=0 ; 五十円玉枚数 < input.五十円玉の枚数();五十円玉枚数++){
+            金 五十円分 = new 金(五十円硬貨,五十円玉枚数);
+            for(int 百円玉枚数 = 0 ; 百円玉枚数 < input.百円玉の枚数();百円玉枚数++){
+                金 百円分 = new 金(百円硬貨,百円玉枚数);
+                for(int 五百円玉枚数 = 0 ;五百円玉枚数 < input.五百円玉の枚数();五百円玉枚数++){
+                    金 五百円分 = new 金(五百円硬貨,五百円玉枚数);
+
+                }
+            }
+        }
+        回答のリスト result = new 回答のリスト(list);
+        return result;
     }
 
     public int 合計金額が予定通りの件数をカウント(入力値 input , 計算結果と内容 value){
@@ -35,15 +49,16 @@ record 回答のリスト(
 ){}
 
 record 計算結果と内容(
-        int 五百円玉の枚数,
-        int 百円玉の枚数,
-        int 五十円玉の枚数,
-        int 合計金額
-){}
+        金 五百円玉の枚数,
+        金 百円玉の枚数,
+        金 五十円玉の枚数
+){
+}
 
 
 
 record 金(
     硬貨 coin
-    ,int number
-){}
+    ,int 枚数
+){
+}
