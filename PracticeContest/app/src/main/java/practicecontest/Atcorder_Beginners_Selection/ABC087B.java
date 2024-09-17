28e3cc1c33bb1e517e9e3692a36edccb67be6c79
Shiryu_Go <ilovenhunhu777@gmail.com>
2024-09-17 23:35:05 +0900

    どうにもうまくいかない。

--- PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC087B.java
+++ PracticeContest/app/src/main/java/practicecontest/Atcorder_Beginners_Selection/ABC087B.java
@@ -5,14 +5,12 @@ import java.util.List;
 import java.util.Scanner;
 import 汎用.硬貨;
 
-import static 汎用.硬貨.*;
-
 public class ABC087B {
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)) {
             入力値 input = new 入力値(sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt());
             回答のリスト 全回答=全件網羅(input);
-            long result = 全回答.回答List().stream().filter(x ->合計金額が予定通りの件数をカウント(input,x)).count();
+            long result = 全回答.回答List().stream().filter(x ->合計金額が予定通り(input,x)).count();
             System.out.println(result);
         }
     }
@@ -20,11 +18,11 @@ public class ABC087B {
     public static 回答のリスト 全件網羅(入力値 input) {
         List<計算結果と内容> list = new ArrayList<>();
         for(int 五十円玉枚数=0 ; 五十円玉枚数 <= input.五十円玉の枚数();五十円玉枚数++){
-            金 五十円分 = new 金(五十円硬貨,五十円玉枚数);
+            金 五十円分 = new 金(硬貨.五十円硬貨,五十円玉枚数);
             for(int 百円玉枚数 = 0 ; 百円玉枚数 <= input.百円玉の枚数();百円玉枚数++){
-                金 百円分 = new 金(百円硬貨,百円玉枚数);
+                金 百円分 = new 金(硬貨.百円硬貨,百円玉枚数);
                 for(int 五百円玉枚数 = 0 ;五百円玉枚数 <= input.五百円玉の枚数();五百円玉枚数++){
-                    金 五百円分 = new 金(五百円硬貨,五百円玉枚数);
+                    金 五百円分 = new 金(硬貨.五百円硬貨,五百円玉枚数);
                     list.add(new 計算結果と内容(五百円分,百円分,五十円分));
                 }
             }
@@ -33,8 +31,8 @@ public class ABC087B {
         return result;
     }
 
-    public static boolean 合計金額が予定通りの件数をカウント(入力値 input , 計算結果と内容 value){
-        return input.合計金額() == value.合計金額の取得();
+    public static boolean 合計金額が予定通り(入力値 input , 計算結果と内容 value){
+        return input.合計金額() == value.合計金額の取得() ;
     }
 }
 
